c2baa92a7aa2be4551a1c0cdb741a95b2133ce08
Alexander <pchp250g8@tutamail.com>
2026-04-13 16:53:17 +0300

    Fixed Source Code

--- OctToDec.py
+++ OctToDec.py
@@ -6,18 +6,18 @@ import math
 nDecNum = 0
 nOctPower = 1
 MAX_INT = 2 ** 32 - 1
+nMaxOctLen = math.ceil(math.log(MAX_INT, 8))
 os.system("cls")
 print("Input an octal number:")
 strLine = input()
 nStrLen = len(strLine)
-nMaxOctLen = math.log(MAX_INT, 8)
 oMatches = regex.fullmatch("^[0-7]+$", strLine)
-bRightString = (nStrLen < nMaxOctLen and oMatches is not None)
+bRightString = (nStrLen <= nMaxOctLen and oMatches is not None)
 if (not bRightString):
     print("Wrong octal number format!!!")
     exit(0)
 for i in range(nStrLen):
-    nBinDigit = ord(strLine[nStrLen - 1 - i]) - ord('0')
-    nDecNum += (nBinDigit * nOctPower)
+    nOctDigit = ord(strLine[nStrLen - 1 - i]) - ord('0')
+    nDecNum += (nOctDigit * nOctPower)
     nOctPower *= 8
 print(f"The decimal equivalent of the octal number {strLine} is {nDecNum}")
